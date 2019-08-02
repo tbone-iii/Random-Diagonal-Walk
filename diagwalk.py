56d95167e7cf62b199f50e8e903478a25672e686
thomas_arambula <thomasarambula96@gmail.com>
2019-08-01 21:39:22 -0700

    Adds allow backtracking option. Adds beeping sound for each plot.

--- diagwalk.py
+++ diagwalk.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-from random import random
+from random import random, choice
 from typing import Tuple
+import winsound
 
 # Initialize number of points
 NUM_OF_POINTS = 10
@@ -10,10 +11,10 @@ NUM_OF_POINTS = 10
 ORIGIN = (0, 0)
 
 # Establish the time difference (essentially, the speed)
-TIME_DELTA_MS = 1000
+TIME_DELTA_MS = 200
 
 # Bool for whether a point that was just previously plotted can be selected
-ALLOW_BACKTRACKING = False
+ALLOW_BACKTRACK = False
 
 # Configure the style for pyplot
 plt.style.use("fivethirtyeight")
@@ -26,30 +27,68 @@ y_vals = [ORIGIN[1]]
 Point = Tuple[float, float]
 
 
-def pick_random_corner(point: Point) -> Point:
+def beep(frequency_hz=1000, duration_ms=100):
+    winsound.Beep(frequency_hz, duration_ms)
+
+
+def pick_random_corner(point: Point, prev_point: Point = None,
+                       allow_backtrack=ALLOW_BACKTRACK) -> Point:
     """ Picks a random corner in one diagonal direction.
 
         The point is determined based off of the given point,
         where the new point is (x+-1, y+-1) randomly selected.
 
+        Point:   the current point to move from
+        prev_point:   the point before the current point
+        allow_backtrack:    whether backtracking to the previous point
+                            is permitted
+
+
         Returns the new point (tuple).
     """
     (x, y) = point
+
+    # Make the random selection of x and y
     if random() < 0.5:
-        x += 1
+        x_new = x + 1
     else:
-        x -= 1
+        x_new = x - 1
 
     if random() < 0.5:
-        y += 1
+        y_new = y + 1
     else:
-        y -= 1
-    return (x, y)
+        y_new = y - 1
+
+    if prev_point is not None:
+        if not allow_backtrack and (x_new, y_new) == prev_point:
+            # Find change in points in (x, y)
+            dx, dy = (x - prev_point[0]), (y - prev_point[1])
+
+            # Create list of possible options for (dx, dy)
+            options = [(dx, -dy), (-dx, dy), (-dx, -dy)]
+
+            # Choose a random option
+            (dx, dy) = choice(options)
+
+            # Determine new (x, y) point when backtracking is disallowed
+            x_new = x + dx
+            y_new = y + dy
+
+    return (x_new, y_new)
 
 
 def animate(i):
     """ Animate function to plot figures in pyplot. """
-    (x_new, y_new) = pick_random_corner((x_vals[-1], y_vals[-1]))
+    # TODO: Fix this mess.
+    # Selects a random corner to plot
+    if i >= 2:
+        prev_point = x_vals[-2], y_vals[-2]
+    else:
+        prev_point = None
+    (x_curr, y_curr) = x_vals[-1], y_vals[-1]
+    (x_new, y_new) = pick_random_corner((x_curr, y_curr), prev_point)
+
+    # Adds the new points to the x, y lists
     x_vals.append(x_new)
     y_vals.append(y_new)
 
@@ -65,6 +104,9 @@ def animate(i):
     # Plot item
     plt.plot(x_vals, y_vals, color="black", linestyle="solid", marker="o",
              markerfacecolor="blue", markersize=5, linewidth=1)
+
+    # Make a beep for each plot
+    beep()
 
 
 def main():
