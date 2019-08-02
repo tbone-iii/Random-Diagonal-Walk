505d8c7b7dde321cc934d7674c19d429905aad20
thomas_arambula <thomasarambula96@gmail.com>
2019-08-01 20:25:50 -0700

    Initial commit

---
+++ diagwalk.py
@@ -0,0 +1,74 @@
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from random import random
+from typing import Tuple
+
+# initialize number of points
+NUM_OF_POINTS = 10
+
+# Establish the origin position; (0, 0) is the typical tuple
+ORIGIN = (0, 0)
+
+# Establish the time difference (essentially, the speed)
+TIME_DELTA_MS = 1000
+
+# Bool for whether a point that was just previously plotted can be selected
+ALLOW_BACKTRACKING = False
+
+# Configure the style for pyplot
+plt.style.use("fivethirtyeight")
+
+# Initialize x and y list of coordinates
+x_vals = [ORIGIN[0]]
+y_vals = [ORIGIN[1]]
+
+
+def pick_random_corner(point: Tuple[float, float]) -> Tuple[float, float]:
+    """ Picks a random corner in one diagonal direction.
+
+        The point is determined based off of the given point,
+        where the new point is (x+-1, y+-1) randomly selected.
+    """
+    (x, y) = point
+    if random() < 0.5:
+        x += 1
+    else:
+        x -= 1
+
+    if random() < 0.5:
+        y += 1
+    else:
+        y -= 1
+    return (x, y)
+
+
+def animate(i):
+    """ Animate function to plot figures in pyplot. """
+    (x_new, y_new) = pick_random_corner((x_vals[-1], y_vals[-1]))
+    x_vals.append(x_new)
+    y_vals.append(y_new)
+
+    # Clear previous plot
+    plt.cla()
+
+    # Set title
+    plt.title(f"Time {TIME_DELTA_MS*i/1000}s")
+
+    # Create equal axes ratio
+    plt.gca().set_aspect('equal', adjustable='box')
+
+    # Plot item
+    plt.plot(x_vals, y_vals, color="black", linestyle="solid", marker="o",
+             markerfacecolor="blue", markersize=5, linewidth=1)
+
+
+def main():
+    """ Main function that plots the random walk. """
+    ani = FuncAnimation(plt.gcf(), animate, interval=TIME_DELTA_MS)
+
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
